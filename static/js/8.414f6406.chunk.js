b72e80615a7a461a4e6f41a96029f0567895cf0c
whoami-anoint <abhishekkfl@gmail.com>
2024-06-19 11:37:22 +0530

    changes

---
+++ static/js/8.414f6406.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-tailwindcss-portfolio"]=this["webpackJsonpreact-tailwindcss-portfolio"]||[]).push([[8],{50:function(e,s,i){"use strict";i.r(s);i(0);var t=i(2);s.default=()=>Object(t.jsxs)("div",{children:[Object(t.jsx)("h2",{children:"Poem Title"}),Object(t.jsx)("p",{children:"This is where your poem goes."})]})}}]);
+//# sourceMappingURL=8.414f6406.chunk.js.map
